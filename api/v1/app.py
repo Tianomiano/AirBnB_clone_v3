eb27c0f9860cca23946a168ebfa9a2a27765f104
Tianomiano <patomiano6@gmail.com>
2023-07-30 07:57:24 +0000

    fixed pep8 errors

--- api/v1/app.py
+++ api/v1/app.py
@@ -24,10 +24,12 @@ def teardown_flask(exception):
     # print(exception)
     storage.close()
 
+
 @app.errorhandler(404)
 def error_404(error):
     '''Handles the 404 HTTP error code.'''
     return jsonify(error='Not found'), 404
+
 
 @app.errorhandler(400)
 def error_400(error):
@@ -36,6 +38,7 @@ def error_400(error):
     if isinstance(error, Exception) and hasattr(error, 'description'):
         msg = error.description
     return jsonify(error=msg), 400
+
 
 if __name__ == '__main__':
     app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
