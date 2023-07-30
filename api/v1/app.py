1305c05d8f04e68df1886d9999e8981df3c5a7ee
Tianomiano <patomiano6@gmail.com>
2023-07-30 07:14:10 +0000

    corrections

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,7 +8,6 @@ from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 
-
 app = Flask(__name__)
 '''The Flask web application instance.'''
 app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
