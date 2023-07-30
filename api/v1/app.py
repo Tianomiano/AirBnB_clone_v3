93f4f2151c22aca509ef9f886363baf512258d67
Tianomiano <patomiano6@gmail.com>
2023-07-30 07:39:44 +0000

    restore from a certain commit

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,6 +8,7 @@ from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 
+
 app = Flask(__name__)
 '''The Flask web application instance.'''
 app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
@@ -23,12 +24,10 @@ def teardown_flask(exception):
     # print(exception)
     storage.close()
 
-
 @app.errorhandler(404)
 def error_404(error):
     '''Handles the 404 HTTP error code.'''
     return jsonify(error='Not found'), 404
-
 
 @app.errorhandler(400)
 def error_400(error):
@@ -37,7 +36,6 @@ def error_400(error):
     if isinstance(error, Exception) and hasattr(error, 'description'):
         msg = error.description
     return jsonify(error=msg), 400
-
 
 if __name__ == '__main__':
     app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
